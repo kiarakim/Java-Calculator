0dacf1642c3f6f89416e1cf7a022b192311ef6d2
kiara kim <kiarakim413@gmail.com>
2022-03-08 23:02:47 +0900

    link eclipse with git

---
+++ godOfJava/src/chapter02/Calculator.java
@@ -0,0 +1,30 @@
+package chapter02;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+        int result;
+        result = a + b;
+        return result;
+    }
+    public int subtract(int a, int b) {
+        int result;
+        result = a - b;
+        return result;
+    }
+    public int multiply(int a, int b) {
+        int result;
+        result = a * b;
+        return result;
+    }
+    public int divide(int a, int b) {
+        int result;
+        result = a / b;
+        return result;
+    }
+	public static void main(String[] args) {
+			
+		
+	}
+
+}
---
+++ godOfJava/src/chapter02/Profile.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+public class Profile {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Kiara");
+		System.out.println("My age is 25");
+	}
+
+}
