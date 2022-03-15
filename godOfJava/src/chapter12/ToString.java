a92da892ed3322eabfcfa198d9257601608d1cd5
kiara kim <kiarakim413@gmail.com>
2022-03-15 20:37:05 +0900

    add chapter12 .java files

---
+++ godOfJava/src/chapter12/ToString.java
@@ -0,0 +1,22 @@
+package chapter12;
+
+public class ToString {
+
+	public static void main(String[] args) {
+		ToString thisObj = new ToString();
+//		thisObj.toStringMethod(thisObj);
+		thisObj.toStringMethod2();
+	}
+	
+	public void toStringMethod(Object obj) {
+		System.out.println(obj);
+		System.out.println(obj.toString());
+		System.out.println("plus " + obj);
+	}
+	
+	public void toStringMethod2() {
+		System.out.println(this);
+		System.out.println(toString());
+		System.out.println("plus " + this);
+	}
+}
