ded2694ecb071eedef5fd7857f05899df232f261
kiara kim <kiarakim413@gmail.com>
2022-03-16 14:14:10 +0900

    add chapter13 .java files

---
+++ godOfJava/src/chapter13/InfoManagerImpl.java
@@ -0,0 +1,20 @@
+package chapter13;
+
+import chapter12.infoDTO;
+
+public class InfoManagerImpl implements InfoManager {
+	@Override
+	public boolean addInfo(infoDTO info) {
+		return false;
+	}
+	
+	@Override 
+	public boolean removeInfo(String name, String phone) {
+		return false;
+	}
+	
+	@Override
+	public boolean updateInfo(infoDTO info) {
+		return false;
+	}
+}
---
+++ godOfJava/src/chapter13/InfoManagerImpl2.java
@@ -0,0 +1,15 @@
+package chapter13;
+
+import chapter12.infoDTO;
+
+public class InfoManagerImpl2 extends InfoManagerAbstract {
+	public boolean addInfo(infoDTO info) {
+		return false;
+	}
+	public boolean removeInfo(String name, String phone){
+		return false;
+	}
+	public boolean updateInfo(infoDTO info){
+		return false;
+	}
+}
