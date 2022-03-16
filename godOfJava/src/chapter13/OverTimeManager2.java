96a923e40bb02e533fd83b3d27a7d591b446d0d0
kiara kim <kiarakim413@gmail.com>
2022-03-16 14:14:31 +0900

    add chapter13 .java files

---
+++ godOfJava/src/chapter13/OverTimeManager2.java
@@ -0,0 +1,11 @@
+package chapter13;
+
+public class OverTimeManager2 {
+
+	public static void main(String[] args) {
+		OverTimeValues2 value2 = OverTimeValues2.FIVR_HOUR;
+		System.out.println(value2);
+		System.out.println(value2.getAmount());
+	}
+
+}
