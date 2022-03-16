cc02d32d7a1b16ea37631bc37d834057dc5bded2
kiara kim <kiarakim413@gmail.com>
2022-03-16 14:14:26 +0900

    add chapter13 .java files

---
+++ godOfJava/src/chapter13/InterfaceExample.java
@@ -0,0 +1,11 @@
+package chapter13;
+
+//import chapter13.InfoManager;
+
+public class InterfaceExample {
+
+	public static void main(String[] args) {
+		InfoManager info = new InfoManagerImpl();
+	}
+
+}
