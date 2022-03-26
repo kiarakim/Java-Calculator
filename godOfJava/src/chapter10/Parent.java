4ec309d05320334569f66f4c06afd07398797ab0
kiara kim <kiarakim413@gmail.com>
2022-03-26 17:31:54 +0900

    add chapter10 .java files

--- godOfJava/src/chapter10/Parent.java
+++ godOfJava/src/chapter10/Parent.java
@@ -1,17 +1,13 @@
 package chapter10;
 
-public class Parent {
-
-//	public static void main(String[] args) {
-//		
-//
-//	}
-	public Parent() {
-		System.out.println("Parent Constructor");
-	}
+	public class Parent {
+		
+		public Parent() {
+			System.out.println("Parent Constructor");
+		}
+		
+		public void printName() {
+			System.out.println("Parent printName()");
+		}
 	
-	public void printName() {
-		System.out.println("Parent printName()");
 	}
-
-}
