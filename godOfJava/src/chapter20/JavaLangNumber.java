3198e0f80f2d199c4124644ddf4d1a7aaa1a3e84
kiara kim <kiarakim413@gmail.com>
2022-04-04 10:47:20 +0900

    add chapter20 .java files

---
+++ godOfJava/src/chapter20/JavaLangNumber.java
@@ -0,0 +1,13 @@
+package chapter20;
+
+public class JavaLangNumber {
+
+	public static void main(String[] args) {
+		JavaLangNumber sample = new JavaLangNumber();
+		sample.numberTypeCheck();
+	}
+	public void numberTypeCheck() {
+			String value1 = "3";
+	}//p.555
+
+}
