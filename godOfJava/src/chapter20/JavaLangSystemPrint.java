61b6f9bece6443f7069f0d48d1ef7c2ce076613e
kiara kim <kiarakim413@gmail.com>
2022-04-11 20:17:14 +0900

    add chapter20 .java files

---
+++ godOfJava/src/chapter20/JavaLangSystemPrint.java
@@ -0,0 +1,35 @@
+package chapter20;
+
+public class JavaLangSystemPrint {
+
+	public static void main(String[] args) {
+		JavaLangSystemPrint sample = new JavaLangSystemPrint();
+		sample.printStreamCheck();
+		sample.printNull();
+		sample.printNullToString();
+	}
+	
+	public void printStreamCheck() {
+		byte b = 127;
+		short s = 32767;
+		System.out.println(b);
+		System.out.println(s);
+		printInt(b);
+		printInt(s);
+	}
+	
+	public void printInt(int value) {
+		System.out.println(value);
+	}
+	
+	public void printNull() {
+		Object obj = null;
+		System.out.println(obj);
+		System.out.println(obj + "is object's value");
+	}
+	
+	public void printNullToString() {
+		Object obj = null;
+		System.out.println(obj.toString());
+	}
+}
