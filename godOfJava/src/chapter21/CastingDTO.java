54b29ddad55f338d32c70bb440a0b201248e1469
kiara kim <kiarakim413@gmail.com>
2022-04-25 22:28:19 +0900

    add chapter21 .java files

--- godOfJava/src/chapter21/CastingDTO.java
+++ godOfJava/src/chapter21/CastingDTO.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class CastingDTO implements Serializable {
 	private Object object;
-	public void setObject(Object object) {
+	public void setObject(Object object) { 
 		this.object = object;
 	}
 	public Object getObject() {
