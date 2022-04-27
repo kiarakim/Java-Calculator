06138c6bcdda82bd38cd03a53c28f30a9ab123d2
kiara kim <kiarakim413@gmail.com>
2022-04-27 23:19:02 +0900

    add chapter21 .java files

--- godOfJava/src/chapter21/GenericSample.java
+++ godOfJava/src/chapter21/GenericSample.java
@@ -24,7 +24,7 @@ public class GenericSample {
 	public void checkDTO(CastingDTO dto) {
 		Object tempObject = dto.getObject();
 		if(tempObject instanceof StringBuilder) {
-			System.out.println("StringBuilder");
+			System.out.println("This is StringBuilder");
 		}else if(tempObject instanceof StringBuffer) {
 			System.out.println("StringBuffer"); 
 		}
