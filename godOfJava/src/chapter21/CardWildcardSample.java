94ad40ee5266a1cb18e9a21a455fc99e3a896a37
kiara kim <kiarakim413@gmail.com>
2022-05-09 18:31:52 +0900

    add chapter21 .java files

--- godOfJava/src/chapter21/CardWildcardSample.java
+++ godOfJava/src/chapter21/CardWildcardSample.java
@@ -3,7 +3,27 @@ package chapter21;
 public class CardWildcardSample {
 
 	public static void main(String[] args) {
-
+		CardWildcardSample sample = new CardWildcardSample(); 
+//		sample.callBoundedWildcardMethod();
+		sample.callBusBoundedWildcardMethod();
+	}
+	/*
+	public void callBoundedWildcardMethod() {
+		WildcardGeneric<Car> wildcard = new WildcardGeneric<Car>();
+		wildcard.setWildcard(new Car("Mustang"));
+		boundedWildcardMethod(wildcard);
+	}
+	*/
+	
+	public void callBusBoundedWildcardMethod() {
+		WildcardGeneric<Bus> wildcard = new WildcardGeneric<Bus>();
+		wildcard.setWildcard(new Bus("6900"));
+		boundedWildcardMethod(wildcard);
+	}
+	
+	public void boundedWildcardMethod(WildcardGeneric<? extends Car> c) {
+		Car value = c.getWildcard();
+		System.out.println(value);
 	}
 
 }
