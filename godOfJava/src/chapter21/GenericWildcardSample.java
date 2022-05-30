746cf1213cda097a51d31bc66c37ac11f0e27d1a
kiara kim <kiarakim413@gmail.com>
2022-05-30 19:24:26 +0900

    add chapter21 .java files

---
+++ godOfJava/src/chapter21/GenericWildcardSample.java
@@ -0,0 +1,21 @@
+package chapter21;
+
+public class GenericWildcardSample {
+
+	public static void main(String[] args) {
+		GenericWildcardSample sample = new GenericWildcardSample();
+		sample.callGenericMethod();
+	}
+	
+	public <T> void genericMethod(WildcardGeneric<T> c, T addValue) {
+		c.setWildcard(addValue);
+		T value = c.getWildcard();
+		System.out.println(value);
+	}
+	
+	public void callGenericMethod() {
+		WildcardGeneric<String> wildcard = new WildcardGeneric<String>();
+		genericMethod(wildcard, "Data");
+	}
+
+}
