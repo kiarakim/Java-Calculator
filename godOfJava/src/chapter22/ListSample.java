91632b24db97334fe0fd10b586a68bf5bb7ba782
kiara kim <kiarakim413@gmail.com>
2022-06-06 20:51:04 +0900

    add chapter22 .java files

--- godOfJava/src/chapter22/ListSample.java
+++ godOfJava/src/chapter22/ListSample.java
@@ -10,7 +10,9 @@ public class ListSample {
 //		sample.checkArrayList3();
 //		sample.checkArrayList4();
 //		sample.checkArrayList5();
-		sample.checkArrayList6();
+//		sample.checkArrayList6();
+//		sample.checkArrayList7();
+		sample.checkArrayList8();
 	}
 //	public void checkArrayList1() {
 //		ArrayList<String> list2 = new ArrayList<String>(100);
@@ -65,7 +67,36 @@ public class ListSample {
 		}
 	}
 	public void checkArrayList6() {
-		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("A");
+		String[] strList = list.toArray(new String[0]);
+		System.out.println(strList);
+	}
+	public void checkArrayList7() {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		String[] tempArray = new String[2];
+		String[] strList = list.toArray(tempArray);
+		for(String temp:strList) {
+			System.out.println(temp);
+		}
+	}
+	public void checkArrayList8() {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		list.add("A");
+//		System.out.println("Removed " + list.remove(0));
+		System.out.println(list.remove("A"));
+//		ArrayList<String> temp = new ArrayList<String>();
+//		temp.add("A");
+//		list.removeAll(temp);
+		for(int loop = 0; loop < list.size(); loop++) {
+			System.out.println("list.get(" + loop + ") = " + list.get(loop));
+		}
 	}
 
 }
